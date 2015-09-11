d47c9cc15b9bab4309091c6e94b9643b4730ae2e
Javier Rodriguez <jaroso78@yahoo.es>
2015-09-12 00:29:57 +0200

    #modify diferentes elements add toggle menu login

--- www/js/app.js
+++ www/js/app.js
@@ -12,19 +12,43 @@ console.log('Librería cargada.');
 
 /*************************CONTROLLER**********************************************************************/
 var controlador = {
-
+     self : this,
 	_$Botton_login : $('#button_login'),
+	_$Secundaria : $('.segundaria'),
 	_inicializarUI : function() {
 		var self = this;
-	 	this._$Botton_login.click(function(evt) {
-           console.log('login');
+		   this._$Botton_login.click(function(evt) {
+
+			   $('#login').animate({"right":"100%"},"slow").addClass('visible')
+
+			/*self._mostrarPantalla(self._$Secundaria);*/
 	 	});
-	}
+	},
+
+	_mostrarPantalla : function($pantallaDestino) {
+		var self = this;
+        var $pantallaOrigen = $('.activa');
+		/*console.log($pantallaDestino);*/
+		$pantallaOrigen.fadeOut(function(){
+			$pantallaOrigen.removeClass('activa');
+			$pantallaDestino.fadeIn(function(){
+				$pantallaDestino.addClass('activa');
+			});
+		});
+        },
+
+
+
 }
+
 
 
 /******************************INICIO*******************************************************************/
 $(document).ready(function () {
+	//Definición Menu login lateral
+
+
+
 
 	controlador._inicializarUI();
 
